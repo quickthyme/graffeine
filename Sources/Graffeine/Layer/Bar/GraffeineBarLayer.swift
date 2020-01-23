27d6e2bf30f3f725655423e70611b7d682659bbb
Quickthyme <quickthyme@mac.com>
2020-01-23 13:22:53 -0600

    improvement to bar layer clipping
    Signed-off-by: Quickthyme <quickthyme@mac.com>

--- Sources/Graffeine/Layer/Bar/GraffeineBarLayer.swift
+++ Sources/Graffeine/Layer/Bar/GraffeineBarLayer.swift
@@ -7,6 +7,8 @@ open class GraffeineBarLayer: GraffeineLayer {
     public var unitSubdivision: GraffeineLayer.UnitSubdivision? = nil
     public var roundedEnds: RoundedEnds = .none
 
+    public var clipLoEdge: Bool = true
+
     override open func generateSublayer() -> CALayer {
         return Bar()
     }
@@ -18,6 +20,8 @@ open class GraffeineBarLayer: GraffeineLayer {
         for (index, bar) in sublayers.enumerated() {
             guard let bar = bar as? Bar, index < numberOfUnits else { continue }
             bar.frame = self.bounds
+            applyLoEdgeClippingMaskIfEnabled()
+
             bar.subdivision = unitSubdivision
             bar.roundedEnds = roundedEnds
             bar.flipXY = flipXY
@@ -37,10 +41,29 @@ open class GraffeineBarLayer: GraffeineLayer {
         }
     }
 
+    open func applyLoEdgeClippingMaskIfEnabled() {
+        guard (clipLoEdge) else { return }
+        let mask = CAShapeLayer()
+        mask.path = UIBezierPath(rect: rectForLoEdgeClippingMask).cgPath
+        mask.fillColor = UIColor.black.cgColor
+        self.mask = mask
+    }
+
+    private var rectForLoEdgeClippingMask: CGRect {
+        return (flipXY)
+            ? CGRect(x: 0,
+                     y: -20,
+                     width: bounds.width + 20,
+                     height: bounds.height + 40)
+            : CGRect(x: -20,
+                     y: -20,
+                     width: bounds.width + 40,
+                     height: bounds.height + 20)
+    }
+
     override public init() {
         super.init()
         self.contentsScale = UIScreen.main.scale
-        self.masksToBounds = true
     }
 
     public convenience init(id: AnyHashable, region: Region = .main) {
@@ -60,6 +83,7 @@ open class GraffeineBarLayer: GraffeineLayer {
             self.unitMargin = layer.unitMargin
             self.unitSubdivision = layer.unitSubdivision
             self.roundedEnds = layer.roundedEnds
+            self.clipLoEdge = layer.clipLoEdge
         }
     }
 
