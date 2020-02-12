d39495c345e91e1ed1e8dfd18d6667b1f6ae0b87
Quickthyme <quickthyme@mac.com>
2020-02-12 00:24:20 -0600

    swiftui selection enabled
    Signed-off-by: Quickthyme <quickthyme@mac.com>

--- Sources/Graffeine/GraffeineView+SwiftUI.swift
+++ Sources/Graffeine/GraffeineView+SwiftUI.swift
@@ -10,10 +10,15 @@ import SwiftUI
 public struct GraffeineViewRep: UIViewRepresentable {
     public typealias UIViewType = GraffeineView
     public var configClass: String?
+
     @Binding public var layerDataInput: [GraffeineView.LayerData]
 
+    public var onSelect: GraffeineView.OnSelect?
+
     public func makeUIView(context: UIViewRepresentableContext<GraffeineViewRep>) -> GraffeineView {
-        return GraffeineView(frame: .zero, configClass: self.configClass ?? "")
+        let view = GraffeineView(frame: .zero, configClass: self.configClass ?? "")
+        view.onSelect = onSelect
+        return view
     }
 
     public func updateUIView(_ uiView: GraffeineView, context: UIViewRepresentableContext<GraffeineViewRep>) {
@@ -21,8 +26,11 @@ public struct GraffeineViewRep: UIViewRepresentable {
         uiView.layoutSublayers(of: uiView.layer)
     }
 
-    public init(configClass: String? = nil, layerDataInput: Binding<[GraffeineView.LayerData]>) {
+    public init(configClass: String? = nil,
+                layerDataInput: Binding<[GraffeineView.LayerData]>,
+                onSelect: GraffeineView.OnSelect? = nil) {
         self.configClass = configClass
         self._layerDataInput = layerDataInput
+        self.onSelect = onSelect
     }
 }
